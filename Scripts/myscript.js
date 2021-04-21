6326bc08a633da613e4debbf5b32281fa134c1e3
zahibe <zahibe@clalit.org.il>
2021-04-21 17:34:12 +0300

    first commit

---
+++ Scripts/myscript.js
@@ -0,0 +1,18 @@
+﻿
+    function printButtonClicked(){
+        var client_name = $("#clientNameId").val() == "" ? "פלוני בן פלונית" : $("#clientNameId").val();
+        var client_id = $("#clientIdInput").val() == "" ? "999999287" : $("#clientIdInput").val();
+        var test_day = $("#testDateId").val() == "" ? "20/12/2020" : $("#testDateId").val();
+        var d = new Date();
+        var month = '' + (d.getMonth() + 1);
+        var day = '' + d.getDate();
+        var year = d.getFullYear();
+        var formatDate = day + "/" + month + "/" + year;
+
+        $('.client_id').text(client_id);
+        $('.client_NameHere').text(client_name);
+        $('.test_date').text(test_day);
+        $('.current_dateHere').text(formatDate)
+
+        window.print();
+    }
